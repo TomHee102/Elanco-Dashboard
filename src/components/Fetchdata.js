2d4cea0b4d31a3d3adec8d62b8588d337b73cbeb
ahmedobaid <aobaid2004@outlook.com>
2023-03-17 16:41:53 +0000

    Fetch data test

---
+++ src/components/Fetchdata.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import React from "react";
+
+export default function Fetchdata() {
+    const [data, setData] = React.useState([]);
+    const fetchdata = async () => {
+        const response = await axios.get("https://sampledata.elancoapps.com/data/plot/plot2");
+        setData(response.data);
+    }
+    React.useEffect(() => {
+        fetchdata();
+    }
+    , []);
+}
+
