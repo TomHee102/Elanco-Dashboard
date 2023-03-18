e6289a18f72fb921172118f7d21eddba2fef0513
goose271 <angusowenparkin@gmail.com>
2023-03-18 23:48:32 +0000

    Added component PlotForm.js for adding new plots

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,20 +2,15 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import { Button } from '@mui/material';
+import PlotForm from './PlotForm.js';
 
 export default function Sidebar() {
-    return (
+  return(
+      <>
         <Box sx={{ boxShadow: 10, position: 'absolute', flexGrow: 1, bgcolor: '#4c93d9', width: 150, height: 1080, textAlign: 'left',}}>
-          <Tabs
-            orientation="vertical"
-            aria-label="vertical tab"
-          >
-            <Tab sx={{"&:hover":{backgroundColor: 'white', boxShadow: 10}}} label="Item One" />
-            <Tab sx={{"&:hover":{backgroundColor: 'white', boxShadow: 10}}} label="Item Two" />
-            <Tab sx={{"&:hover":{backgroundColor: 'white', boxShadow: 10}}} label="Item Three" />
-            <Tab sx={{"&:hover":{backgroundColor: 'white', boxShadow: 10}}} label="Item Four" />
-            <Tab sx={{"&:hover":{backgroundColor: 'white', boxShadow: 10}}} label="Item Five" />
-          </Tabs>
+          <div><PlotForm /></div>         
         </Box>
-    )
+      </>
+    );
 }
