174712580ed7bcdae9feb22c8a2e74b80bc6c3aa
Ao-baid <aobaid2004@outlook.com>
2023-04-03 22:46:31 +0100

    Crop count shown by piechart. Layout not final and subject to change.

--- src/components/Outputdata.js
+++ src/components/Outputdata.js
@@ -2,91 +2,146 @@ import React, { useEffect } from "react";
 import { Line, Scatter } from "react-chartjs-2";
 import Fetchdata from "./Fetchdata";
 import { Bar } from "react-chartjs-2"
-import crops from '../data/Crop.json';
-
-
+import cropData from '../data/Crop.json';
+import { Pie } from "react-chartjs-2";
 
 
 function PlotAnalysis(props) {
-    const chartData = props.chartData;
+  const chartData = props.chartData;
+  const cropsData = cropData;
+  var profit = 0;
   
-    // Define the crop type and its ranges
-    const cropType = "Wheat";
-    const ranges = {
-      ph: [6, 6.8],
-      temp: [20, 30],
-      humidity: [40, 60],
-      light: [35, 65],
+  // Define the crop type and its ranges
+  const ranges = {};
+  cropsData.forEach(crop => {
+    ranges[crop.cropType] = {
+      ph: crop.phRange,
+      temp: crop.tempRange,
+      humidity: crop.humidityRange,
+      light: crop.lightRange,
+      costAndMaintenance: crop.costAndMaintenance,
+      yield: crop.yield,
+      growthTimeInDays: crop.growthTimeInDays
     };
-  
-    // Analyze each data point and calculate the profit
-    const analyzedData = chartData.map((data) => {
-      // Get the date and data points for this data point
-      const { date, ph, temp, humidity, light } = data;
-  
-      // Check if the data points fall within the corresponding range
-      const isWithinRange =
-        ph.every((value) => value >= ranges.ph[0] && value <= ranges.ph[1]) &&
-        temp.every((value) => value >= ranges.temp[0] && value <= ranges.temp[1]) &&
-        humidity.every(
-          (value) => value >= ranges.humidity[0] && value <= ranges.humidity[1]
-        ) &&
-        light.every((value) => value >= ranges.light[0] && value <= ranges.light[1]);
-  
-      // Calculate the profit if the data points are within range
-      const costAndMaintenance = "£100";
-      const yield_ = "£450";
-      const growthTimeInDays = 45;
-      const profit =
-        isWithinRange &&
-        parseFloat(yield_.slice(1)) -
-          parseFloat(costAndMaintenance.slice(1)) /
-            (365 / growthTimeInDays);
-  
-      return {
-        date,
-        isWithinRange,
-        profit,
-      };
-    });
+  });
 
-    console.log(analyzedData)
+  // initialize the crop count object
+  const cropCount = {};
+
+  //iterate through chartData and ranges, compare the values and return the crop type for each data point
+  const cropType = chartData.map((data) => {
+    
+    for (const crop in ranges) {
+      if (
+        data.ph >= ranges[crop].ph[0] &&
+        data.ph <= ranges[crop].ph[1] &&
+        data.temp >= ranges[crop].temp[0] &&
+        data.temp <= ranges[crop].temp[1] &&
+        data.humidity >= ranges[crop].humidity[0] &&
+        data.humidity <= ranges[crop].humidity[1] &&
+        data.light >= ranges[crop].light[0] &&
+        data.light <= ranges[crop].light[1]
+      ) {
+        // increment the count for the crop
+        if (cropCount[crop]) {
+          cropCount[crop]++;
+        } else {
+          cropCount[crop] = 1;
+        }
+
+        //convert yield to number, consider that there is a $ sign in the data
+        profit = Number(ranges[crop].yield.replace(/[^0-9.-]+/g,"")) - Number(ranges[crop].costAndMaintenance.replace(/[^0-9.-]+/g,""));
+
+        return (
+          //return the name of the crop and the yield next to it'
+          <div key={crop}>
+            <h3>Crop Type: {crop}</h3>
+            <h3>Yield: {ranges[crop].yield}</h3>
+            <h3>costAndMaintenance: {ranges[crop].costAndMaintenance}</h3>
+          </div>
+        );
+      }
+    }
+  });
+
+  // create a piechart with the different crops that can be grown in the plot
+  const cropCountItems = Object.entries(cropCount).map(([crop, count]) => (
+    <div key={crop}>
+      <h3>Crop Type: {crop}</h3>
+      <h3>Count: {count}</h3>
+    </div>
+  ));
+
+  const cropLabels = Object.keys(cropCount);
+  const cropValues = Object.values(cropCount);
+
+  const pieChart = (
+    <Pie
+      data={{
+        labels: cropLabels,
+        datasets: [
+          {
+            data: cropValues,
+            label: "Crop Count",
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      }}
+      height={400}
+      width={600}
+    />
+  );
+
+
+  return (
+    <div>
+      {cropType}
+      <h1>Crop Count</h1>
+      {cropCountItems}
+      {pieChart}
+    </div>
+  );
+}
+
+
+
+
   
-    // Render the analyzed data
-    return (
-      <div>
-        <h2>Plot Analysis</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Within Range</th>
-              <th>Profit</th>
-              <th>Crop</th>
-            </tr>
-          </thead>
-          <tbody>
-            {analyzedData.map((data) => (
-              <tr key={data.date}>
-                <td>{data.date}</td>
-                <td>{data.isWithinRange ? "Yes" : "No"}</td>
-                <td>{data.profit ? `£${data.profit.toFixed(2)}` : "-"}</td>
-                <td>{cropType}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <h1>Total Profit</h1>
-        <h2>
-            £
-            {analyzedData
-                .filter((data) => data.profit)
-                .reduce((total, data) => total + data.profit, 0)
-                .toFixed(2)}
-        </h2>
-      </div>
-    );
-  }
+
+
+  
+  
+
+  
+
+
+  
+
+  
+
+  
+  
+
+
+
+
+
   
 
 export default function Outputdata({plot}) {
